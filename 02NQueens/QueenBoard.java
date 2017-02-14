a3755df1b80f4884ab677aa8bdb0cf7f91b90159
BermetKalmakova <bermetkalmakova@gmail.com>
2017-02-13 23:32:02 -0500

    finally done

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,11 +1,13 @@
 public class QueenBoard{
 
     private int[][]board;
-    private int solutionCount = 0;
+    private int solutionCount = -1;
+    private int side;
 
     public QueenBoard(int size){
 	board = new int[size][size];
 	empty(size);
+	side = size;
     }
 	    
     private void empty(int num){
@@ -29,41 +31,131 @@ public class QueenBoard{
     {
 	return solveH(0);
     }
-
     private boolean solveH(int col){
-	for(int row = 0; row < board[].length(); row++){
+	for(int row = 0; row < side; row++){    
 	    if(board[col][row] == 0){
 		addQueen(col,row);
-		if(col == board[].length() - 1){
-		    solutionCount ++;
+		if(col == side-1){
+		    return true;
 		}
-		solveH(col+1);
-		removeQueen(col,row);
+		else{
+		    if(solveH(col+1)){
+			return true;
+		    }
+		    else{
+			removeQueen(col,row);
+		    }
+		}			
 	    }
 	}
-	return solutionCount > 0;
+	return false;
     }
-    private void addQueen(int col, int row){
+    public void addQueen(int col, int row){
 	board[col][row] = -1;
-	for (int i = 0; i < board[].length(); i++){
+	for (int i = 0; i < side; i++){
 	    if(board[col][i] > -1){
 		board[col][i] += 1;
 	    }
 	    if(board[i][row] > -1){
 		board[i][row] += 1;
 	    }
+	    try{
+		if(board[col+i][row+i] > -1){
+		    board[col+i][row+i] += 1;
+		}
+	    }
+	    catch(IndexOutOfBoundsException e){
+	    }
+	    try{
+		if(board[col-i][row-i] > -1){
+		    board[col-i][row-i] += 1;
+		}
+	    }
+	    catch(IndexOutOfBoundsException e){
+	    }
+	    try{
+		if(board[col+i][row-i] > -1){
+		    board[col+i][row-i] += 1;
+		}
+	    }
+	    catch(IndexOutOfBoundsException e){
+	    }
+	    try{
+		if(board[col-i][row+i] > -1){
+		    board[col-i][row+i] += 1;
+		}
+	    }
+	    catch(IndexOutOfBoundsException e){
+	    }
 	}
 	
     }
 
-    private void removeQueen(int col, int row){
+    public void removeQueen(int col, int row){
+	for (int i = 0; i < side; i++){
+	    if(board[col][i] > -1){
+		board[col][i] -= 1;
+	    }
+	    if(board[i][row] > -1){
+		board[i][row] -= 1;
+	    }
+	    try{
+		if(board[col+i][row+i] > -1){
+		    board[col+i][row+i] -= 1;
+		}
+	    }
+	    catch(IndexOutOfBoundsException e){
+	    }
+	    try{
+		if(board[col-i][row-i] > -1){
+		    board[col-i][row-i] -= 1;
+		}
+	    }
+	    catch(IndexOutOfBoundsException e){
+	    }
+	    try{
+		if(board[col+i][row-i] > -1){
+		    board[col+i][row-i] -= 1;
+		}
+	    }
+	    catch(IndexOutOfBoundsException e){
+	    }
+	    try{
+		if(board[col-i][row+i] > -1){
+		    board[col-i][row+i] -= 1;
+		}
+	    }
+	    catch(IndexOutOfBoundsException e){
+	    }
+	    board[col][row] = 0;
+	}
     }
     /**
      *@return the number of solutions found, or -1 if the board was never solved.
      *The board should be reset after this is run.    
      */
+    public void countSolutions(){
+	empty(side);
+	solutionCount = 0;
+	count(0);
+    }
     public int getSolutionCount(){
     	return solutionCount;
+    }
+    private void count(int col){
+	for(int row = 0; row < side; row++){    
+	    if(board[col][row] == 0){
+		addQueen(col,row);
+		if(col == side-1){
+		    solutionCount++;
+		}
+		else{
+		    count(col + 1);
+		}
+		removeQueen(col,row);
+		
+	    }			
+	}
     }
 
     /**toString
@@ -72,20 +164,37 @@ public class QueenBoard{
      */
     public String toString(){
 	String ans = "";
-	if (solve()){
-	    for(int col = 0; col<board[].length();col++){
-		for(int row = 0; row<board[].length();row++){
-		    if (board[row][col] == -1){
-			ans += "Q";
-		    }
-		    else{
-			ans += "_";
-		    }
+	for(int col = 0; col<side;col++){
+	    for(int row = 0; row<side;row++){
+		if (board[row][col] == -1){
+		    ans += "Q";
 		}
-		ans += "\n";
+		else{
+		    ans += "_";
+		}
 	    }
-	    return ans;
+	    ans += "\n";
 	}
-        return "none";
+	return ans;
+    }
+    public String toStringt(){
+	String ans = "";
+	for(int col = 0; col<side;col++){
+	    for(int row = 0; row<side;row++){
+		ans += board[col][row];
+	    }
+	    ans += "\n";
+	}
+	return ans;
+    }
+    public static void main(String[] args){
+	QueenBoard a = new QueenBoard(5);
+	//a.addQueen(5,6);
+	//a.removeQueen(5,6);
+        
+        a.solve();
+	System.out.println(a.toString());
+        a.countSolutions();
+	System.out.println(a.getSolutionCount());
     }
 }
