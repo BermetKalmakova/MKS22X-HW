6a7b30c1d7e8c0d722ab00cd3c1263ddacc67992
BermetKalmakova <bermetkalmakova@gmail.com>
2017-02-15 14:46:18 -0500

    work on recursive function

--- 03Knights/KnightBoard.java
+++ 03Knights/KnightBoard.java
@@ -2,12 +2,31 @@ public class KnightBoard{
     private int[][]board;
     
     public KnightBoard(int startingRows, int startingCols){
+	board = new int[startingRows][startingCols];
+	fill(startingRows,startingCols);
+    }
+    private void fill(int a, int b){
+	for(int i = 0; i < a; i ++){
+	    for(int j = 0; j < b; j ++){
+		board[i][j] = 0;
+	    }
+	}
     }
     
     public String toString(){
     }
     public void solve(){
+	solveH(0,0,1)
     }
     private boolean solveH(int row, int col, int level){
+	private int[] a = {1,2};
+	private int[] b = {1,2};
+	if(level > board.length * board[].length){
+	    return true;
+	}
+	if(board[row][col] == 0){
+	    board[row][col] = level;
+	    
+	}
     }
 }
