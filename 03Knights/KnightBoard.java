edf2f39cb2cd062acfa505d461672934cffbd557
BermetKalmakova <bermetkalmakova@gmail.com>
2017-02-15 14:26:27 -0500

    add empty methods

--- 03Knights/KnightBoard.java
+++ 03Knights/KnightBoard.java
@@ -1,2 +1,13 @@
 public class KnightBoard{
+    private int[][]board;
+    
+    public KnightBoard(int startingRows, int startingCols){
+    }
+    
+    public String toString(){
+    }
+    public void solve(){
+    }
+    private boolean solveH(int row, int col, int level){
+    }
 }
