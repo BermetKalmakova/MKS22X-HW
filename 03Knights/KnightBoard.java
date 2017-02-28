e8f4bf50e2c45a6b3183d60d0cf22613291e7ea3
BermetKalmakova <bermetkalmakova@gmail.com>
2017-02-27 23:33:12 -0500

    finish Knight's tour

--- 03Knights/KnightBoard.java
+++ 03Knights/KnightBoard.java
@@ -12,7 +12,7 @@ public class KnightBoard{
 	    for(int j = 0; j < b; j ++){
 		board[i][j] = 0;
 		numPossible[i][j] = 0;
-		countPossible[i][j];
+		countPossible(i,j);
 	    }
 	}
     }
@@ -23,119 +23,198 @@ public class KnightBoard{
 		numPossible[a][b] ++;
 	    }
 	}
+	catch(IndexOutOfBoundsException e){
+	}
 	try{
 	    if(board[a+2][b-1] == 0){
 		numPossible[a][b] ++;
 	    }
+	}
+	catch(IndexOutOfBoundsException e){
 	}
 	try{
 	    if(board[a-2][b+1] == 0){
 		numPossible[a][b] ++;
 	    }
 	}
+	catch(IndexOutOfBoundsException e){
+	}
 	try{
 	    if(board[a-2][b-1] == 0){
 		numPossible[a][b] ++;
 	    }
+	}
+	catch(IndexOutOfBoundsException e){
 	}
 	try{
 	    if(board[a+1][b+2] == 0){
 		numPossible[a][b] ++;
 	    }
 	}
+	catch(IndexOutOfBoundsException e){
+	}
 	try{
 	    if(board[a-1][b+2] == 0){
 		numPossible[a][b] ++;
 	    }
+	}
+	catch(IndexOutOfBoundsException e){
 	}
 	try{
 	    if(board[a+1][b-2] == 0){
 		numPossible[a][b] ++;
 	    }
 	}
+	catch(IndexOutOfBoundsException e){
+	}
 	try{
 	    if(board[a-1][b-2] == 0){
 		numPossible[a][b] ++;
 	    }
 	}
+	catch(IndexOutOfBoundsException e){
+	}
     }
     
     public String toString(){
+	String ans = "";
+	for(int col = 0; col<board[0].length;col++){
+	    for(int row = 0; row<board.length;row++){
+		if(board[row][col] < 10){
+		    ans += " ";
+		}
+		ans += board[row][col] + " ";
+	    }
+	    ans += "\n";
+	}
+	return ans;
     }
     public void solve(){
-	solveH(0,0,1)
+	solveH(0,0,1);
     }
     private int newR;
     private int newC;
     private boolean solveH(int row, int col, int level){
-	private int[] a = {1,2};
-	private int[] b = {1,2};
-	if(level > board.length * board[].length){
+	if(level > board.length * board[0].length){
 	    return true;
 	}
 	if(board[row][col] == 0){
 	    board[row][col] = level;
-	    if(tryMoves(row,col)){
-		if(solveH(newR,newC,level+1)){
-		    return true;
-		}
+	    
+	    if(tryMoves(row,col,level) || level == board.length * board[0].length){
+		return true;
 	    }
-	    board[row][col] = 0;
+	    else{
+		board[row][col] = 0;
+		//level--;
+	    }
 	    
 	}
 	return false;
     }
     //figure out which choice has the least possiblities and change numPossible to reflect that nvm
-    private boolean tryMoves(int a,int b){
+    private boolean tryMoves(int a,int b, int level){
 	try{
 	    if(board[a+2][b+1] == 0){
 		numPossible[a][b] ++;
 		newR = a+2;
 		newC = b+1;
-		return true;
+	        if(solveH(newR,newC,level+1)){
+		    return true;
+		}
 	    }
+	}
+	catch(IndexOutOfBoundsException e){
 	}
 	try{
 	    if(board[a+2][b-1] == 0){
 		numPossible[a][b] ++;
 		newR = a+2;
-		newC = b+1;
-		return true;
+		newC = b-1;
+		if(solveH(newR,newC,level+1)){
+		    return true;
+		}
 	    }
+	}
+	catch(IndexOutOfBoundsException e){
 	}
 	try{
 	    if(board[a-2][b+1] == 0){
 		numPossible[a][b] ++;
-		newR = a+2;
+		newR = a-2;
 		newC = b+1;
-		return true;
+		if(solveH(newR,newC,level+1)){
+		    return true;
+		}
 	    }
+	}
+	catch(IndexOutOfBoundsException e){
 	}
 	try{
 	    if(board[a-2][b-1] == 0){
 		numPossible[a][b] ++;
+		newR = a-2;
+		newC = b-1;
+		if(solveH(newR,newC,level+1)){
+		    return true;
+		}
 	    }
+	}
+	catch(IndexOutOfBoundsException e){
 	}
 	try{
 	    if(board[a+1][b+2] == 0){
 		numPossible[a][b] ++;
+		newR = a+1;
+		newC = b+2;
+		if(solveH(newR,newC,level+1)){
+		    return true;
+		}
 	    }
+	}
+	catch(IndexOutOfBoundsException e){
 	}
 	try{
 	    if(board[a-1][b+2] == 0){
 		numPossible[a][b] ++;
+		newR = a-1;
+		newC = b+2;
+		if(solveH(newR,newC,level+1)){
+		    return true;
+		}
 	    }
+	}
+	catch(IndexOutOfBoundsException e){
 	}
 	try{
 	    if(board[a+1][b-2] == 0){
 		numPossible[a][b] ++;
+		newR = a+1;
+		newC = b-2;
+		if(solveH(newR,newC,level+1)){
+		    return true;
+		}
 	    }
+	}
+	catch(IndexOutOfBoundsException e){
 	}
 	try{
 	    if(board[a-1][b-2] == 0){
 		numPossible[a][b] ++;
+		newR = a-1;
+		newC = b-2;
+		if(solveH(newR,newC,level+1)){
+		    return true;
+		}
 	    }
 	}
+	catch(IndexOutOfBoundsException e){
+	}
 	return false;
+    }
+    public static void main(String[] args){
+	KnightBoard a = new KnightBoard(7,7);
+	a.solve();
+	System.out.println(a.toString());
     }
 }
