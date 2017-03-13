4a7d5a9cb264cba8f1748069beb96ce0f00310bf
BermetKalmakova <bermetkalmakova@gmail.com>
2017-03-13 02:14:36 -0400

    cannot find bug in silver

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -77,21 +77,106 @@ public class USACO{
 	return sum * 72 * 72;
     }
     public int silver(String filename){
-	return 0;
-    }
-    // public String toString(int[][] ex){
-    // 	String ans = "";
-    // 	for(int col = 0; col<ex.length;col++){
-    // 	    for(int row = 0; row<ex[0].length;row++){
-    // 		ans += ex[col][row];
-    // 	    }
-    // 	    ans += "\n";
-    // 	}
-    // 	return ans;
-    // }
-    // public static void main(String[] args){
-    // 	USACO x = new USACO(); //does not have to do anything. 
+	int N = -1;
+	int M = -1;
+	int T = -1;
+	int R1 = -1;
+	int C1 = -1;
+	int R2 = -1;
+	int C2 = -1;
+	char[][] grid;
+	int[][][] moves;
+	int ans = 0;
+	try{
+	    File x = new File(filename);
+	    Scanner file = new Scanner(x).useDelimiter("\\r?\\n");
+	    ArrayList<String> lines = new ArrayList<String>();
+	    while (file.hasNext()){
+		lines.add(file.next());
+	    }
+	    String[] line1 = lines.get(0).split("\\s+");
+	    N = Integer.parseInt(line1[0]);
+	    M = Integer.parseInt(line1[1]);
+	    T = Integer.parseInt(line1[2]);
 
-    // 	System.out.println (x.bronze("makelake.in"));
-    // }
+	    String[] linecoord = lines.get(N+1).split("\\s+");
+	    R1 = Integer.parseInt(linecoord[0]);
+	    C1 = Integer.parseInt(linecoord[1]);
+	    R2 = Integer.parseInt(linecoord[2]);
+	    C2 = Integer.parseInt(linecoord[3]);
+
+	    grid = new char[N][M];
+	    moves = new int[T][N][M];
+	    for(int i = 0;i < N; i++){
+		for(int j = 0; j < M; j++){
+		    grid[i][j] = lines.get(i+1).charAt(j);
+		    if(grid[i][j] == '.'){
+			moves[0][i][j] = 0;
+		    }
+		    else{
+			moves[0][i][j] = -1;
+		    }
+		}
+	    }
+	    moves[0][R1-1][C1-1] = 1;
+	    for(int a = 1; a < T+1; a++){
+		for(int b = 0; b < N; b++){
+		    for(int c = 0; c < M; c++){
+			int sum = 0;
+			try{
+			    if(moves[a-1][b+1][c] > -1){
+				sum += moves[a-1][b+1][c];
+			    }
+			}
+			catch(IndexOutOfBoundsException e){
+			}
+			try{
+			    if(moves[a-1][b-1][c] > -1){
+				sum += moves[a-1][b-1][c];
+			    }
+			}
+			catch(IndexOutOfBoundsException e){
+			}
+			try{
+			    if(moves[a-1][b][c+1] > -1){
+				sum += moves[a-1][b][c+1];
+			    }
+			}
+			catch(IndexOutOfBoundsException e){
+			}
+			try{
+			    if(moves[a-1][b][c-1] > -1){
+				sum += moves[a-1][b][c-1];
+			    }
+			}
+			catch(IndexOutOfBoundsException e){
+			}
+			moves[a][b][c] = sum;
+			System.out.println(toString(moves));
+		    }
+		}
+	    }
+	    ans = moves[T][R2-1][C2-1];
+	}
+	catch(FileNotFoundException d){
+	}
+	return ans;
+    }
+    public String toString(int[][][] ex){
+    	String ans = "";
+	for (int t = 0; t < ex.length;t++){
+	    for(int col = 0; col<ex[0].length;col++){
+		for(int row = 0; row<ex[0][0].length;row++){
+		    ans += ex[t][col][row];
+		}
+		ans += "\n";
+	    }
+	}
+    	return ans;
+    }
+    public static void main(String[] args){
+    	USACO x = new USACO(); //does not have to do anything. 
+
+    	System.out.println (x.silver("ctravel.in"));
+    }
 }
