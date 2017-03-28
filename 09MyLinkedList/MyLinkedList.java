c41e6be22c802ad78d481bfd14a9b6d8aa7dc37b
BermetKalmakova <bermetkalmakova@gmail.com>
2017-03-28 14:48:39 -0400

    start

---
+++ 09MyLinkedList/MyLinkedList.java
@@ -0,0 +1,17 @@
+public class MyLinkedList{
+    LNode start;
+    int size;
+    public MyLinkedList(){
+	size = 0;
+    }
+    public boolean add(int value){
+    }
+    public int size(){
+    }
+    public String toString(){
+    }
+    public int get(int index){
+    }
+    public int set(int index, int newValue){
+    }
+}
