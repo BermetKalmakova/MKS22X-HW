44773c7d2e9752c7744856a03ca804a9cd14fff2
BermetKalmakova <bermetkalmakova@gmail.com>
2017-03-30 14:44:30 -0400

    progress

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Merge{
     public static void merge(int[]a, int[]b, int[]destination){
 	int i = 0;
@@ -29,12 +30,26 @@ public class Merge{
 	    i++;
 	}
     }
-    public st
+    public static void mergesort(int[]ary){
+	if (ary.length >= 1){
+	    return;
+	}
+	else{
+	    int[]left = Arrays.copyOfRange(ary,0,ary.length/2);
+	    int[]right = Arrays.copyOfRange(ary,ary.length/2,ary.length);
+	    mergesort(left);
+	    mergesort(right);
+	    merge(left,right,ary);
+	}
+    }
     public static void main(String[] args){
-	int[] ex1 = {1,2,3,3,4,4,7,9};
+	int[] ex1 = {7,4,90,3,6,12,4,75,34,0,7,4,5,7,3,2,564,756,47,896,3,2,4,56,54};
 	int[] ex2 = {3,3,5,6,7,10};
 	int[] end = new int[14];
-	merge(ex1,ex2,end);
-	System.out.println(end);
+	//rge(ex1,ex2,end);
+	mergesort(ex1);
+	for(int i = 0; i < ex1.length; i++){
+	    System.out.println(ex1[i]);
+	}
     }
 }
