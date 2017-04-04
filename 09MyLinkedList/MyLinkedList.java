832d3decaa60638269a06206c2a0d7c30705fa63
BermetKalmakova <bermetkalmakova@gmail.com>
2017-04-04 14:49:15 -0400

    add

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,17 +1,64 @@
+java.util.*;
 public class MyLinkedList{
-    LNode start;
+    private class LNode{
+	LNode next,prev;
+	int value;
+	public LNode(int value){
+	    this.value = value;
+	}
+	public String toString(){
+	    return value + "";
+	}
+	//get set
+    }
+    LNode head,tail;
     int size;
     public MyLinkedList(){
 	size = 0;
     }
-    public boolean add(int value){
-    }
     public int size(){
+	return size;
     }
-    public String toString(){
+    private LNode getNthNode(int n){
+	LNode node = head;
+	int i = 0;
+	while (node != null && i < index){
+	    if(i == index){
+		return node;
+	    }
+	    i++;
+	    node = node.next;
+	} 
     }
+
+
+    private void addAfter(LNode location, LNode toBeAdded){/*complete this*/  }
+
+
+    private void remove(LNode target){/*complete this*/}
+
+
+
+    public String toString(){ /*complete this*/  }
+
+    public boolean add(int value){
+	add(size,value);
+	return true;
+    }
+
     public int get(int index){
+        return getNthNode(index).value;
     }
-    public int set(int index, int newValue){
+
+    public int set(int index, int value){
+	getNthNode(index).value = this.value;
     }
+
+    public int indexOf(int value){/*complete this*/}
+
+    public int remove(int index){
+	remove(getNthNode(index));
+    }
+
+    public void add(int index,int value){/*complete this*/}
 }
