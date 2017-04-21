20fdb7373266e00dfed43f1054e1c7a7de07ad8d
BermetKalmakova <bermetkalmakova@gmail.com>
2017-04-21 14:34:50 -0400

    done

--- HW/eval.java
+++ HW/eval.java
@@ -2,16 +2,16 @@ import java.util.*;
 public class eval{
     public static double eval(String s){
 	String[] tokens = s.split(" ");
-	Stack values = new Stack();
+	Stack<String> values = new Stack<String>();
 	for(String token:tokens){
 	    if(isOp(token)){
-		values.push(apply(token,Double.parseDouble(values.pop()),Double.parseDouble(values.pop())));
+		values.push(apply(token,Double.parseDouble(values.pop()),Double.parseDouble(values.pop()))+"");
 	    }
 	    else{
 		values.push(token);
 	    }
 	}
-	return Double.parseDouble(values.pop());
+	return Double.parseDouble((String)values.pop());
     }
     public static boolean isOp(String x){
 	return x.equals("+") || x.equals("-") || x.equals("*") || x.equals("/") || x.equals("%");
@@ -29,11 +29,11 @@ public class eval{
 	if(op.equals("/")){
 	    return a / b;
 	}
-	if(op.equals("%")){
-	    return a % b;
-	}
+	return a % b;
     }
     public static void main(String[] args){
 	System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));
+	System.out.println(eval("11 3 - 4 + 2.5 *"));
+	System.out.println(eval("10 2.0 +"));
     }
 }
