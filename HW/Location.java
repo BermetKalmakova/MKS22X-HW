ebf2f6fc80814acba19a50d9da28cb8ce5cd5dc1
BermetKalmakova <bermetkalmakova@gmail.com>
2017-05-16 14:41:39 -0400

    start

---
+++ HW/Location.java
@@ -0,0 +1,10 @@
+public class Location implements Comparable<Location>{
+    private int row,col,distToGoal,distToStart;
+    private Location previous;
+    private boolean aStar;
+    
+    public Location(int r,int c,Location previous,int distToStart,int distToGoal,boolean aStar){
+    }
+    public int CompareTo(Location other){
+    }
+}
---
+++ HW/MazeSolver.java
@@ -0,0 +1,15 @@
+public class MazeSolver{
+    public MazeSolver(String filename){
+	this(filename,false);
+    }
+    public MazeSolver(String filename, boolean animate){
+    }
+
+    public void solve(){
+	solve(1);
+    }
+    public void solve(int style){
+    }
+    public String toString(){
+    }
+}
