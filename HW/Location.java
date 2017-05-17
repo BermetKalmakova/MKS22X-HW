8ed487828f9bbcca61448de7caa66671260693d3
BermetKalmakova <bermetkalmakova@gmail.com>
2017-05-17 14:49:08 -0400

    progress

--- HW/Location.java
+++ HW/Location.java
@@ -4,7 +4,23 @@ public class Location implements Comparable<Location>{
     private boolean aStar;
     
     public Location(int r,int c,Location previous,int distToStart,int distToGoal,boolean aStar){
+	row = r;
+	col = c;
+	this.previous = previous;
+	this.distToStart = distToStart;
+	this.distToGoal = distToGoal;
+	aStar = aStar;
+    }
+    public int getdts(){
+	return distToStart;
+    }
+    public int getdtg(){
+	return distToGoal;
     }
     public int CompareTo(Location other){
+	if(aStar){
+	    return (distToStart + distToGoal) - (other.getdts + other.getdtg);
+	}
+        return distToGoal - other.dtg;
     }
 }
