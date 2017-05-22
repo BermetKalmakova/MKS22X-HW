906c49b27275514c7784efc46768763d38bdeaa4
BermetKalmakova <bermetkalmakova@gmail.com>
2017-05-22 14:13:03 -0400

    add

--- HW/FrontierQueue.java
+++ HW/FrontierQueue.java
@@ -1,2 +1,9 @@
 public class FrontierQueue implements Frontier{
+    private Queue<Location> locations;
+    public void add(Location l){
+	locations.add(l);
+    }
+    public Location next(){
+	return locations.remove();
+    }
 }
--- HW/FrontierStack.java
+++ HW/FrontierStack.java
@@ -1,2 +1,9 @@
 public class FrontierStack implements Frontier{
+    private Stack<Location> locations;
+    public void add(Location l){
+	locations.push(l);
+    }
+    public Location next(){
+	return locations.pop();
+    }
 }
